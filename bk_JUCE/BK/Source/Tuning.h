54630bca9207b00a01c9beefc6c8814385a31e9e
Daniel Trueman <dan@airfiddle.home>
2016-12-09 20:02:03 -0500

    updating tuning files

--- bk_JUCE/BK/Source/Tuning.h
+++ bk_JUCE/BK/Source/Tuning.h
@@ -1,17 +1,125 @@
 /*
-  ==============================================================================
-
-    Tuning.h
-    Created: 4 Dec 2016 3:40:02pm
-    Author:  Daniel Trueman
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ Tuning.h
+ Created: 4 Dec 2016 3:40:02pm
+ Author:  Daniel Trueman
+ 
+ ==============================================================================
+ */
 
 #ifndef TUNING_H_INCLUDED
 #define TUNING_H_INCLUDED
 
 #include "BKUtilities.h"
+
+#include "AudioConstants.h"
+
+class TuningPreparation : public ReferenceCountedObject
+{
+public:
+    typedef ReferenceCountedObjectPtr<TuningPreparation>   Ptr;
+    typedef Array<TuningPreparation::Ptr>                  Arr;
+    typedef Array<TuningPreparation::Ptr, CriticalSection> CSArr;
+    
+    TuningPreparation(TuningSystem whichTuning,
+                      PitchClass fundamental,
+                      TuningSystem adaptiveIntervalScale,
+                      bool adaptiveInversional,
+                      TuningSystem adaptiveAnchorScale,
+                      PitchClass adaptiveAnchorFundamental,
+                      uint64 adaptiveClusterThresh,
+                      int adaptiveHistory,
+                      Array<float> customScale):
+    tWhichTuning(whichTuning),
+    tFundamental(fundamental),
+    tAdaptiveIntervalScale(adaptiveIntervalScale),
+    tAdaptiveInversional(adaptiveInversional),
+    tAdaptiveAnchorScale(adaptiveAnchorScale),
+    tAdaptiveAnchorFundamental(adaptiveAnchorFundamental),
+    tAdaptiveClusterThresh(adaptiveClusterThresh),
+    tAdaptiveHistory(adaptiveHistory),
+    tCustom(customScale)
+    {
+        
+    }
+    
+    TuningPreparation():
+    tWhichTuning(EqualTemperament),
+    tFundamental(C),
+    tAdaptiveIntervalScale(JustTuning),
+    tAdaptiveInversional(true),
+    tAdaptiveAnchorScale(EqualTemperament),
+    tAdaptiveAnchorFundamental(C),
+    tAdaptiveClusterThresh(100),
+    tAdaptiveHistory(4),
+    tCustom({0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.})
+    {
+        
+    }
+    
+    ~TuningPreparation()
+    {
+        
+    }
+    
+    
+    inline const TuningSystem getTuning() const noexcept                    {return tWhichTuning;               }
+    inline const PitchClass getFundamental() const noexcept                 {return tFundamental;               }
+    inline const TuningSystem getAdaptiveIntervalScale() const noexcept     {return tAdaptiveIntervalScale;     }
+    inline const bool getAdaptiveInversional() const noexcept               {return tAdaptiveInversional;       }
+    inline const TuningSystem getAdaptiveAnchorScale() const noexcept       {return tAdaptiveAnchorScale;       }
+    inline const PitchClass getAdaptiveAnchorFundamental() const noexcept   {return tAdaptiveAnchorFundamental; }
+    inline const uint64 getAdaptiveClusterThresh() const noexcept           {return tAdaptiveClusterThresh;     }
+    inline const int getAdaptiveHistory() const noexcept                    {return tAdaptiveHistory;           }
+    inline const Array<float> getCustomScale() const noexcept               {return tCustom;                    }
+    
+    
+    inline void setTuning(TuningSystem tuning)                                      {tWhichTuning = tuning;                                 }
+    inline void setFundamental(PitchClass fundamental)                              {tFundamental = fundamental;                            }
+    inline void setAdaptiveIntervalScale(TuningSystem adaptiveIntervalScale)        {tAdaptiveIntervalScale = adaptiveIntervalScale;        }
+    inline void setAdaptiveInversional(bool adaptiveInversional)                    {tAdaptiveInversional = adaptiveInversional;            }
+    inline void setAdaptiveAnchorScale(TuningSystem adaptiveAnchorScale)            {tAdaptiveAnchorScale = adaptiveAnchorScale;            }
+    inline void setAdaptiveAnchorFundamental(PitchClass adaptiveAnchorFundamental)  {tAdaptiveAnchorFundamental = adaptiveAnchorFundamental;}
+    inline void setAdaptiveClusterThresh(uint64 adaptiveClusterThresh)              {tAdaptiveClusterThresh = adaptiveClusterThresh;        }
+    inline void setAdaptiveHistory(int adaptiveHistory)                             {tAdaptiveHistory = adaptiveHistory;                    }
+    inline void setCustomScale(Array<float> tuning)                                 {tCustom = tuning;                                      }
+    
+    
+    void print(void)
+    {
+        DBG("tWhichTuning: " +                  String(tWhichTuning));
+        DBG("tFundamental: " +                  String(tFundamental));
+        DBG("tAdaptiveIntervalScale: " +        String(tAdaptiveIntervalScale));
+        DBG("tAdaptiveInversional: " +          String(tAdaptiveInversional));
+        DBG("tAdaptiveAnchorScale: " +          String(tAdaptiveAnchorScale));
+        DBG("tAdaptiveAnchorFundamental: " +    String(tAdaptiveAnchorFundamental));
+        DBG("tAdaptiveClusterThresh: " +        String(tAdaptiveClusterThresh));
+        DBG("tAdaptiveHistory: " +              String(tAdaptiveHistory));
+        DBG("tCustom: " +                       floatArrayToString(tCustom));
+    }
+private:
+    
+    // basic tuning settings, for static tuning
+    TuningSystem    tWhichTuning;               //which tuning system to use
+    PitchClass      tFundamental;               //fundamental for tuning system
+    
+    // adaptive tuning params
+    TuningSystem    tAdaptiveIntervalScale;     //scale to use to determine successive interval tuning
+    bool            tAdaptiveInversional;       //treat the scale inversionally?
+    
+    TuningSystem    tAdaptiveAnchorScale;       //scale to tune new fundamentals to when in anchored
+    PitchClass      tAdaptiveAnchorFundamental; //fundamental for anchor scale
+    
+    uint64          tAdaptiveClusterThresh;     //ms; max time before fundamental is reset
+    int             tAdaptiveHistory;           //cluster max; max number of notes before fundamental is reset
+    
+    // custom scale
+    Array<float>    tCustom = {0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.}; //custom scale
+    
+    JUCE_LEAK_DETECTOR(TuningPreparation);
+};
+
 
 class TuningProcessor : public ReferenceCountedObject
 {
@@ -20,67 +128,61 @@ public:
     typedef Array<TuningProcessor::Ptr>                  Arr;
     typedef Array<TuningProcessor::Ptr, CriticalSection> CSArr;
     
-    TuningProcessor();
+    TuningProcessor(TuningPreparation::Ptr prep, int layer);;
     ~TuningProcessor();
     
-    void setGlobalOffset(float offset) { globalOffset = offset;}
-    const float getGlobalOffset(void) const noexcept {return globalOffset;}
-    
-    //returns offsets
+    //returns tuning offsets; add to integer PitchClass
     float getOffset(int midiNoteNumber, TuningSystem tuning, PitchClass basepitch) const;
     
     //for calculating adaptive tuning
     void keyOn(int midiNoteNumber);
     
+    //for cluster timing
+    void incrementAdaptiveClusterTime(int numSamples);
+    
+    //for global tuning adjustment, A442, etc...
+    void setGlobalTuningReference(float tuningRef) { globalTuningReference = tuningRef;}
+    const float getGlobalTuningReference(void) const noexcept {return globalTuningReference;}
+    
 private:
     
-    /* Array of all the default tunings
-        Just:       1/1,    16/15,  9/8,    6/5,   5/4,    4/3,    7/5,    3/2,    8/5,    5/3,    7/4,    15/8
-        Partial:    1/1,    16/15,  9/8,    7/6,   5/4,    4/3,    11/8,   3/2,    13/8,   5/3,    7/4,    11/6
-        Duodene:    1/1,    16/15,  9/8,    6/5,   5/4,    4/3,    45/32,  3/2,    8/5,    5/3,    16/9,   15/8
-        Otonal:     1/1,    17/16,  9/8,    19/16, 5/4,    21/16,  11/8,   3/2,    13/8,   27/16,  7/4,    15/8
-        Utonal:     1/1,    16/15,  8/7,    32/27, 16/13,  4/3,    16/11,  32/21,  8/5,    32/19,  16/9,   32/17
-    */
     Array<Array<float>> tuningLibrary;
+    
+    TuningPreparation::Ptr preparation;
+    int layer;
+    
+    float   intervalToRatio(float interval) const noexcept { return mtof(interval + 60.) / mtof(60.); }
+    
+    //adaptive tuning functions
+    float   calculateRatio(int midiNoteNumber);
+    void    newNote(int midiNoteNumber, TuningSystem tuningType);
+    float   calculate(int midiNoteNumber, int anchored);
+    uint64  clusterTime;
+    
+    
+    float   lastNote[128];
+    int     fundamentalNote; //moves with adaptive tuning
+    float   fundamentalFreq;
+    int     historyCounter;
+    bool    reset;
+    
+    float globalTuningReference = 440.; //A440
+    
+    /* Array of all the default tunings
+     Just:       1/1,    16/15,  9/8,    6/5,   5/4,    4/3,    7/5,    3/2,    8/5,    5/3,    7/4,    15/8
+     Partial:    1/1,    16/15,  9/8,    7/6,   5/4,    4/3,    11/8,   3/2,    13/8,   5/3,    7/4,    11/6
+     Duodene:    1/1,    16/15,  9/8,    6/5,   5/4,    4/3,    45/32,  3/2,    8/5,    5/3,    16/9,   15/8
+     Otonal:     1/1,    17/16,  9/8,    19/16, 5/4,    21/16,  11/8,   3/2,    13/8,   27/16,  7/4,    15/8
+     Utonal:     1/1,    16/15,  8/7,    32/27, 16/13,  4/3,    16/11,  32/21,  8/5,    32/19,  16/9,   32/17
+     */
     Array<float> tEqualTuning       = {0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.};
     Array<float> tJustTuning        = {0., .117313, .039101,  .156414, -.13686, -.019547, -.174873, .019547, .136864, -.15641, -.311745, -.11731};
     Array<float> tPartialTuning     = {0., .117313, .039101, -.331291, -.13686, -.019547, -.486824, .019547, .405273, -.15641, -.311745, -.506371};
     Array<float> tDuodeneTuning     = {0., .117313, .039101, .156414, -.13686, -.019547, -.097763, .019547, .136864, -.15641, -.039101, -.11731};
     Array<float> tOtonalTuning      = {0., .049553, .039101, -.02872, -.13686, -.292191, -.486824, .019547, .405273, .058647, -.311745, -.11731};
     Array<float> tUtonalTuning      = {0., .117313, .311745, .156414, -.405273, -.019547, .486824, .292191, .136864, .024847, -.039101,  -.049553};
-
-    
-    //adaptive tuning functions
-    float   intervalToRatio(float interval) const { return mtof(interval + 60.) / mtof(60.); }
-    float   adaptiveTuning_calculateRatio(int midiNoteNumber);
-    void    adaptiveTuning_newNote(int midiNoteNumber, TuningSystem tuningType);
-    float   adaptiveTuning_calculate(int midiNoteNumber, int anchored);
-    void    adaptiveTuning_clusterTimer();
-    
-    /*
-     60				 	=>  int   	adaptiveTuning_fundamentalNote; //moves with adaptive tuning
-     Std.mtof(60.) 		=>  float 	adaptiveTuning_fundamentalFreq;
-     60				 	=>  int 	adaptiveTuning_anchorFundamental; //fixed; when in anchored mode, this determines where new fundamentals are initially tuned
-     partialTuning 		@=> float 	adaptiveTuning_anchorScale[];
-     partialTuning 		@=> float 	adaptiveTuning_intervalScale[];
-     0 					=>  int 	adaptiveTuning_reset;
-     [60.,Std.mtof(60.)] @=> float 	adaptive_lastNote[]; //remembers last key played and frequency sounded
-     
-     500::ms 			=> dur adaptiveTuning_clusterThresh;
-     0::ms   			=> dur adaptiveTuning_clusterTime;
-     spork 				~ adaptiveTuning_clusterTimer();
-     
-     4 					=> int adaptiveTuning_history;
-     0 					=> int adaptiveTuning_historyCounter;
-     0 					=> int adaptiveTuning_inversional; //set intervals so, say, down a seventh is the same as up a seventh (instead of being same as up a step and then transposed down an octave).
-     
-     float 				lastNote[128];
-     for(0=>int i;i<128;i++) 60.0 => lastNote[i];
-     */
-    float globalOffset;
     
     JUCE_LEAK_DETECTOR(TuningProcessor);
 };
-
 
 #endif  // TUNING_H_INCLUDED
