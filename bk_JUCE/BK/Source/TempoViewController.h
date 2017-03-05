5ec279fbdc804e074990fbc56c7a6a46f029821a
Daniel Trueman <dtrueman@princeton.edu>
2017-03-05 09:29:33 -0500

    added tempoViewController. files

---
+++ bk_JUCE/BK/Source/TempoViewController.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    TempoViewController.h
+    Created: 5 Mar 2017 9:28:27am
+    Author:  Daniel Trueman
+
+  ==============================================================================
+*/
+
+#ifndef TEMPOVIEWCONTROLLER_H_INCLUDED
+#define TEMPOVIEWCONTROLLER_H_INCLUDED
+
+
+
+
+
+#endif  // TEMPOVIEWCONTROLLER_H_INCLUDED
