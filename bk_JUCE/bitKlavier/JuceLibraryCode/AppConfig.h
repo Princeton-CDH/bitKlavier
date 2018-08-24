e72283f8ab5252391f2f528694138acb0cbfa564
Dan Trueman <dtrueman@princeton.edu>
2018-08-24 12:08:51 -0400

    changes to JUCE file for App Store release

--- bk_JUCE/bitKlavier/JuceLibraryCode/AppConfig.h
+++ bk_JUCE/bitKlavier/JuceLibraryCode/AppConfig.h
@@ -357,13 +357,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  1
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                2.2.beta2
+ #define JucePlugin_Version                2.2
 #endif
 #ifndef  JucePlugin_VersionCode
  #define JucePlugin_VersionCode            0x20200
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "2.2.beta2"
+ #define JucePlugin_VersionString          "2.2"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
