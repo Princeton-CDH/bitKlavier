b54f80f0b76624698b644b9253f6906c593dd7ca
mulshine <mulshine@princeton.edu>
2019-01-08 13:17:51 -0500

    Windows builds working.

--- bk_JUCE/bitKlavier/JuceLibraryCode/JuceHeader.h
+++ bk_JUCE/bitKlavier/JuceLibraryCode/JuceHeader.h
@@ -45,7 +45,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "bitKlavier";
     const char* const  companyName    = "manyarrowsmusic";
-    const char* const  versionString  = "2.3-beta3";
+    const char* const  versionString  = "2.3";
     const int          versionNumber  = 0x20300;
 }
 #endif
