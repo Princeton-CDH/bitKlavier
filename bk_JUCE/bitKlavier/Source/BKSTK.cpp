b5cba7d895c3397cb09c6b39fbeb30cdc8684063
Theo Trevisan <theotrevisan@gmail.com>
2019-08-23 15:10:13 -0400

    Added BKSTK.h/cpp

---
+++ bk_JUCE/bitKlavier/Source/BKSTK.cpp
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    BKSTK.cpp
+    Created: 22 Aug 2019 1:24:15pm
+    Author:  theot
+
+  ==============================================================================
+*/
+
+#include "BKSTK.h"
+
+BKDelay::BKDelay(BKDelay::Ptr d):
+	delayLinear(d->getDelay()),
+	dSmooth(d->getDSmooth()),
+	dDelayMax(d->getDelayMax()),
+	dDelayGain(d->getDelayGain()),
+	dDelayLength(d->getDelayLength()),
+	dSmoothValue(d->getSmoothValue()),
+	dSmoothDuration(d->getSmoothDuration()),
+	dBlendronicActive(d->getActive())
+{
+}
+
+BKDelay::BKDelay(float delayMax, float delayGain, float delayLength, float smoothValue, float smoothDuration, bool active) :
+	dDelayMax(delayMax),
+	dDelayGain(delayGain),
+	dDelayLength(delayLength),
+	dSmoothValue(smoothValue),
+	dSmoothDuration(smoothDuration),
+	dBlendronicActive(active)
+{
+	delayLinear = stk::DelayL::DelayL(dDelayLength, dDelayMax);
+	dSmooth = stk::Envelope::Envelope();
+	delayLinear.setGain(dDelayGain);
+	dSmooth.setValue(dSmoothValue);
+	dSmooth.setTime(dSmoothDuration);
+}
+
+inline void BKDelay::updateValues()
+{
+	delayLinear.setDelay(dDelayLength);
+	delayLinear.setMaximumDelay(dDelayMax);
+	delayLinear.setGain(dDelayGain);
+	dSmooth.setValue(dSmoothValue);
+	dSmooth.setTime(dSmoothDuration);
+}
+
+inline void BKDelay::updateDelayFromSmooth()
+{
+	dDelayLength = dSmoothValue;
+	delayLinear.setDelay(dSmoothValue);
+}
---
+++ bk_JUCE/bitKlavier/Source/BKSTK.h
@@ -0,0 +1,70 @@
+/*
+  ==============================================================================
+
+    BKSTK.h
+    Created: 22 Aug 2019 1:24:15pm
+    Author:  Theodore R Trevisan
+
+  ==============================================================================
+*/
+
+#ifndef BKSTK_INCLUDED
+#define BKSTK_INCLUDED
+
+#include "BKUtilities.h"
+#pragma once
+
+/*
+////////////////////////////////////////////////////////////////////////////////
+   this will eventually incorporate the replacement for the STK classes but for
+   now it's going to be a wrapper of the delay line-related variables with an ID
+   that will match the blendronomer's ID
+////////////////////////////////////////////////////////////////////////////////
+*/
+
+class BKDelay : public ReferenceCountedObject
+{
+public:
+	typedef ReferenceCountedObjectPtr<BKDelay>   Ptr;
+	typedef Array<BKDelay::Ptr>                  PtrArr;
+	typedef Array<BKDelay::Ptr, CriticalSection> CSPtrArr;
+	typedef OwnedArray<BKDelay>                  Arr;
+	typedef OwnedArray<BKDelay, CriticalSection> CSArr;
+
+	//constructors
+	BKDelay(BKDelay::Ptr d);
+	BKDelay(float delayMax, float delayGain, float delayLength, float smoothValue, float smoothDuration, bool active = false);
+
+	//accessors
+	inline stk::DelayL getDelay() const noexcept { return delayLinear; }
+	inline stk::Envelope getDSmooth() const noexcept { return dSmooth; }
+	inline const float getDelayMax() const noexcept { return dDelayMax; }
+	inline const float getDelayGain() const noexcept { return dDelayGain; }
+	inline const float getDelayLength() const noexcept { return dDelayLength; }
+	inline const float getSmoothValue() const noexcept { return dSmoothValue; }
+	inline const float getSmoothDuration() const noexcept { return dSmoothDuration; }
+	inline const bool getActive() const noexcept { return dBlendronicActive; }
+
+	//mutators
+	inline void updateValues();
+	inline void updateDelayFromSmooth();
+	inline void setDelayMax(float delayMax) { dDelayMax = delayMax; }
+	inline void setDelayGain(float delayGain) { dDelayGain = delayGain; }
+	inline void setDelayLength(float delayLength) { dDelayLength = delayLength; }
+	inline void setSmoothValue(float smoothValue) { dSmoothValue = smoothValue; }
+	inline void setSmoothDuration(float smoothDuration) { dSmoothDuration = smoothDuration; }
+	inline const void setActive(bool newActive) { dBlendronicActive = newActive; }
+	inline const void toggleActive() { dBlendronicActive = !dBlendronicActive; }
+
+private:
+	stk::DelayL delayLinear;
+	stk::Envelope dSmooth;
+	float dDelayLength;
+	float dDelayMax;
+	float dDelayGain;
+	float dSmoothValue;
+	float dSmoothDuration;
+	bool dBlendronicActive;
+};
+
+#endif
