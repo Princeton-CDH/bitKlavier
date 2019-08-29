215f763443164bf6b87fa30f0be99a2aadceec98
Theo Trevisan <theotrevisan@gmail.com>
2019-08-29 19:03:05 -0400

    currently throws out of bounds errors

--- bk_JUCE/bitKlavier/Source/BKSTK.cpp
+++ bk_JUCE/bitKlavier/Source/BKSTK.cpp
@@ -10,6 +10,125 @@
 
 #include "BKSTK.h"
 
+BKEnvelope::BKEnvelope() :
+	value(0.0f),
+	duration(0.0f),
+	rate(0.001f)
+{
+	state = 0;
+}
+
+BKEnvelope::BKEnvelope(float bValue, float bDuration) :
+	value(bValue),
+	duration(bDuration),
+	rate(0.001f)
+{
+	state = 0;
+}
+
+BKEnvelope::~BKEnvelope()
+{
+}
+
+float BKEnvelope::tick()
+{
+	if (state == 1) {
+		if (duration > value) {
+			value += rate;
+			if (value >= duration) {
+				value = duration;
+				state = 0;
+			}
+		}
+		else {
+			value -= rate;
+			if (value <= duration) {
+				value = duration;
+				state = 0;
+			}
+		}
+	}
+
+	return value;
+}
+
+BKDelayL::BKDelayL() :
+	max(4095),
+	gain(1.0),
+	inPoint(0),
+	outPoint(0),
+	doNextOut(false)
+{
+	setLength(0.0);
+}
+
+BKDelayL::BKDelayL(float delayLength, float delayMax, float delayGain) :
+	max(delayMax),
+	gain(delayGain),
+	inPoint(0),
+	outPoint(0),
+	doNextOut(false)
+{
+	setLength(delayLength);
+}
+
+BKDelayL::~BKDelayL()
+{
+}
+
+inline void BKDelayL::setLength(float delayLength)
+{
+	float outPointer = inPoint - length;
+	length = delayLength;
+	while (outPointer < 0) outPointer += inputs.size();
+
+	outPoint = (long)outPointer; //integer part
+	alpha = outPointer - outPoint; //fractional part
+	omAlpha = (float)1.0 - alpha;
+	if (outPoint == inputs.size()) outPoint = 0;
+	doNextOut = true;
+}
+
+float BKDelayL::nextOut()
+{
+	if (doNextOut)
+	{
+		nextOutput = inputs[outPoint] * omAlpha;
+		if (outPoint + 1 < inputs.size())
+			nextOutput += inputs[outPoint + 1] * alpha;
+		else
+			nextOutput += inputs[0] * alpha;
+		doNextOut = false;
+	}
+
+	return nextOutput;
+}
+
+//allows addition of samples without incrementing delay position value
+void BKDelayL::addSample(float input, unsigned long offset)
+{
+	inputs.set(inPoint + offset, input * gain);
+}
+
+float BKDelayL::tick(float input)
+{
+	inputs.set(inPoint++, input * gain);
+	if (inPoint = inputs.size()) inPoint = 0;
+
+	lastFrame.set(0, nextOut());
+	doNextOut = true;
+
+	if (++outPoint == inputs.size()) outPoint = 0;
+
+	return lastFrame[0];
+}
+
+void BKDelayL::scalePrevious(float coefficient, unsigned long offset)
+{
+	if (inputs.size() > 0 && inPoint + offset < inputs.size()) inputs.set((inPoint + offset) % inputs.size(), inputs[inPoint + offset % inputs.size()] * coefficient);
+}
+
+
 BKDelay::BKDelay(BKDelay::Ptr d):
 	delayLinear(d->getDelay()),
 	dSmooth(d->getDSmooth()),
@@ -30,26 +149,27 @@ BKDelay::BKDelay(float delayMax, float delayGain, float delayLength, float smoot
 	dSmoothDuration(smoothDuration),
 	dBlendronicActive(active)
 {
-    delayLinear = std::make_shared<stk::DelayL>(dDelayLength, dDelayMax);
-	dSmooth = std::make_shared<stk::Envelope>();
-	delayLinear->setGain(dDelayGain);
-	dSmooth->setValue(dSmoothValue);
-	dSmooth->setTime(dSmoothDuration);
+	delayLinear =  new BKDelayL(dDelayLength, dDelayMax, dDelayGain);
+	dSmooth = new BKEnvelope(dSmoothValue, dSmoothDuration);
+}
+
+BKDelay::~BKDelay()
+{
 }
 
 void BKDelay::updateValues()
 {
-	delayLinear->setDelay(dDelayLength);
-	delayLinear->setMaximumDelay(dDelayMax);
+	delayLinear->setLength(dDelayLength);
+	delayLinear->setMax(dDelayMax);
 	delayLinear->setGain(dDelayGain);
 	dSmooth->setValue(dSmoothValue);
-	dSmooth->setTime(dSmoothDuration);
+	dSmooth->setDuration(dSmoothDuration);
 }
 
 void BKDelay::updateDelayFromSmooth()
 {
 	dDelayLength = dSmoothValue;
-	delayLinear->setDelay(dSmoothValue);
+	delayLinear->setLength(dSmoothValue);
 }
 
 void BKDelay::addSample(float sampleToAdd, unsigned long offset)
--- bk_JUCE/bitKlavier/Source/BKSTK.h
+++ bk_JUCE/bitKlavier/Source/BKSTK.h
@@ -16,9 +16,97 @@
 
 /*
 ////////////////////////////////////////////////////////////////////////////////
+   bitKlavier replacement for STK DelayL - limited implementation
+////////////////////////////////////////////////////////////////////////////////
+*/
+
+class BKDelayL : public ReferenceCountedObject
+{
+public:
+	typedef ReferenceCountedObjectPtr<BKDelayL>   Ptr;
+	typedef Array<BKDelayL::Ptr>                  PtrArr;
+	typedef Array<BKDelayL::Ptr, CriticalSection> CSPtrArr;
+	typedef OwnedArray<BKDelayL>                  Arr;
+	typedef OwnedArray<BKDelayL, CriticalSection> CSArr;
+
+	//constructors
+	BKDelayL();
+	BKDelayL(float delayLength, float delayMax, float delayGain);
+	~BKDelayL();
+
+	//accessors
+	inline const float getLength() const noexcept { return length; }
+	inline const float getMax() const noexcept { return max; }
+	inline const float getGain() const noexcept { return gain; }
+	inline const float lastOut() const noexcept { return lastFrame[0]; }
+
+	//mutators
+	inline void setLength(float delayLength);
+	inline void setMax(float delayMax) { max = delayMax; }
+	inline void setGain(float delayGain) { gain = delayGain; }
+
+	float nextOut();
+	void addSample(float input, unsigned long offset);
+	float tick(float input);
+	void scalePrevious(float coefficient, unsigned long offset);
+
+private:
+	Array<float> inputs;
+	Array<float> lastFrame;
+	unsigned long inPoint;
+	unsigned long outPoint;
+	float length;
+	float max;
+	float gain;
+	float alpha;
+	float omAlpha;
+	float nextOutput;
+	bool doNextOut;
+};
+
+/*
+////////////////////////////////////////////////////////////////////////////////
+   bitKlavier replacement for STK envelope - limited implementation
+////////////////////////////////////////////////////////////////////////////////
+*/
+
+class BKEnvelope : public ReferenceCountedObject
+{
+public:
+	typedef ReferenceCountedObjectPtr<BKEnvelope>   Ptr;
+	typedef Array<BKEnvelope::Ptr>                  PtrArr;
+	typedef Array<BKEnvelope::Ptr, CriticalSection> CSPtrArr;
+	typedef OwnedArray<BKEnvelope>                  Arr;
+	typedef OwnedArray<BKEnvelope, CriticalSection> CSArr;
+
+	//constructors
+	BKEnvelope();
+	BKEnvelope(float bValue, float bDuration);
+	~BKEnvelope();
+
+	//accessors
+	inline const float getValue() const noexcept { return value; }
+	inline const float getDuration() const noexcept { return duration; }
+	inline const int getState() const noexcept { return state; }
+
+	//mutators
+	inline void setValue(float envelopeValue) { value = envelopeValue; }
+	inline void setDuration(float envelopeDuration) { duration = envelopeDuration; }
+	inline void setRate(float sr) { rate = sr; }
+
+	float tick();
+
+private:
+	float value;
+	float duration;
+	float rate;
+	int state;
+};
+
+/*
+////////////////////////////////////////////////////////////////////////////////
    this will eventually incorporate the replacement for the STK classes but for
-   now it's going to be a wrapper of the delay line-related variables with an ID
-   that will match the blendronomer's ID
+   now it's going to be a wrapper of the delay line and related variables
 ////////////////////////////////////////////////////////////////////////////////
 */
 
@@ -34,10 +122,11 @@ public:
 	//constructors
 	BKDelay(BKDelay::Ptr d);
 	BKDelay(float delayMax, float delayGain, float delayLength, float smoothValue, float smoothDuration, bool active = false);
+	~BKDelay();
 
 	//accessors
-    inline std::shared_ptr<stk::DelayL> getDelay() const noexcept { return delayLinear; }
-	inline std::shared_ptr<stk::Envelope> getDSmooth() const noexcept { return dSmooth; }
+    inline const BKDelayL::Ptr getDelay() const noexcept { return delayLinear; }
+	inline const BKEnvelope::Ptr getDSmooth() const noexcept { return dSmooth; }
 	inline const float getDelayMax() const noexcept { return dDelayMax; }
 	inline const float getDelayGain() const noexcept { return dDelayGain; }
 	inline const float getDelayLength() const noexcept { return dDelayLength; }
@@ -58,8 +147,8 @@ public:
 	inline const void toggleActive() { dBlendronicActive = !dBlendronicActive; }
 
 private:
-    std::shared_ptr<stk::DelayL> delayLinear;
-	std::shared_ptr<stk::Envelope> dSmooth;
+    BKDelayL::Ptr delayLinear;
+	BKEnvelope::Ptr dSmooth;
 	float dDelayLength;
 	float dDelayMax;
 	float dDelayGain;
