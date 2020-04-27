771382787cb1ba74bcd65b3c33f4f0c0f0c87d93
Daniel Trueman <dtrueman@princeton.edu>
2020-04-27 14:34:25 -0400

    updated version num

--- bk_JUCE/bitKlavier/JuceLibraryCode/AppConfig.h
+++ bk_JUCE/bitKlavier/JuceLibraryCode/AppConfig.h
@@ -380,13 +380,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  1
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                2.5.2b6
+ #define JucePlugin_Version                2.5.2
 #endif
 #ifndef  JucePlugin_VersionCode
  #define JucePlugin_VersionCode            0x20502
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "2.5.2b6"
+ #define JucePlugin_VersionString          "2.5.2"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
--- bk_JUCE/bitKlavier/JuceLibraryCode/JuceHeader.h
+++ bk_JUCE/bitKlavier/JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "bitKlavier";
     const char* const  companyName    = "Many Arrows Music";
-    const char* const  versionString  = "2.5.2b6";
+    const char* const  versionString  = "2.5.2";
     const int          versionNumber  = 0x20502;
 }
 #endif
