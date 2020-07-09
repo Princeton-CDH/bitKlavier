59bd38d5fa19672e1b7ef5535b95791bab51a626
Daniel Trueman <dtrueman@princeton.edu>
2020-07-09 17:19:44 -0400

    updated version num to 2.6rc1

--- bk_JUCE/bitKlavier/JuceLibraryCode/JuceHeader.h
+++ bk_JUCE/bitKlavier/JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "bitKlavier";
     const char* const  companyName    = "Many Arrows Music";
-    const char* const  versionString  = "2.5.3b14";
-    const int          versionNumber  = 0x20503;
+    const char* const  versionString  = "2.6rc1";
+    const int          versionNumber  = 0x20600;
 }
 #endif
