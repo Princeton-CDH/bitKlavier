36f2029706cf9c1a57ed49c70f9aae0279dbaaac
Dan Trueman <dtrueman@princeton.edu>
2021-02-14 16:09:48 -0500

    updated to v2.8.1 for release

--- bk_JUCE/bitKlavier/JuceLibraryCode/JucePluginDefines.h
+++ bk_JUCE/bitKlavier/JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  1
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                2.8.1b
+ #define JucePlugin_Version                2.8.1
 #endif
 #ifndef  JucePlugin_VersionCode
  #define JucePlugin_VersionCode            0x20801
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "2.8.1b"
+ #define JucePlugin_VersionString          "2.8.1"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
