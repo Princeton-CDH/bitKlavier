a88daa542528569d5b944f2b43134c59153b71f3
dantrueman <dtrueman@princeton.edu>
2023-05-28 09:29:32 -0400

    updated createInputStream from deprecated form

--- bk_JUCE/bitKlavier/Source/PluginProcessor.cpp
+++ bk_JUCE/bitKlavier/Source/PluginProcessor.cpp
@@ -1651,7 +1651,9 @@ void BKAudioProcessor::importSoundfont(void)
                          {
                              auto url = results.getReference (0);
                              
-                             std::unique_ptr<InputStream> wi (url.createInputStream (false));
+                             std::unique_ptr<InputStream> wi (url.createInputStream (URL::InputStreamOptions (URL::ParameterHandling::inAddress)
+                                                                                     .withConnectionTimeoutMs (1000)
+                                                                                     .withNumRedirectsToFollow (0)));
                              
                              if (wi != nullptr)
                              {
@@ -1691,7 +1693,9 @@ void BKAudioProcessor::importCurrentGallery(void)
          {
              auto url = results.getReference (0);
             
-             if (url.createInputStream (false) != nullptr)
+             if (url.createInputStream (URL::InputStreamOptions (URL::ParameterHandling::inAddress)
+                                        .withConnectionTimeoutMs (1000)
+                                        .withNumRedirectsToFollow (0)) != nullptr)
              {
                  std::shared_ptr<XmlElement> xml = url.readEntireXmlStream();
                 
