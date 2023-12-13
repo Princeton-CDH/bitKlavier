d0372660af4ce63041b0cd1f1ffb9b2c54412ff5
dantrueman <dtrueman@princeton.edu>
2023-12-13 13:39:41 -0500

    fixed bug in nostalgic where reset was causing clicks in undertow notes

--- bk_JUCE/bitKlavier/Source/Nostalgic.cpp
+++ bk_JUCE/bitKlavier/Source/Nostalgic.cpp
@@ -743,7 +743,8 @@ void NostalgicProcessor::keyPressed(int noteNumber, Array<float>& targetVelociti
                                            64,
                                            aGlobalGain,
                                            prep->getGainPtr(),
-                                           false, // THIS NEEDS TO BE FALSE ATM TO GET RID OF UNDERTOW NOTES : because our processsPiano doesn't distinguish between true keyOn/Offs from keyboard and these under the hood keyOn/Offs (which don't correspond with Off/On from keyboard)
+                                           true,
+                                           //false, // THIS NEEDS TO BE FALSE ATM TO GET RID OF UNDERTOW NOTES : because our processsPiano doesn't distinguish between true keyOn/Offs from keyboard and these under the hood keyOn/Offs (which don't correspond with Off/On from keyboard)
                                            true); // true for nostalgicOff
                         }
                     }
